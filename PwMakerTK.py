8b0cce89ba8f6fdc7be1dc6b02047a64b7e8545f
zoeacarter <zoe.carter@bison.howard.edu>
2023-11-27 17:45:22 -0500

    Added 2 buttons

--- PwMakerTK.py
+++ PwMakerTK.py
@@ -6,18 +6,32 @@ import secrets
 class App(tk.Frame):
     def __init__(self, master = None):
         tk.Frame.__init__(self,master)
-        self.grid()
+        self.pack(padx=200, pady =200)
+        
+        
         self.createWidgets()
         
     def createWidgets(self):
         top = self.winfo_toplevel()
         top.rowconfigure(0, weight = 1)
+        top.rowconfigure(1, weight = 1)
         top.columnconfigure(0, weight= 1)
+        top.columnconfigure(1, weight= 1)
         self.rowconfigure(0, weight=1)
-        self.quit = Button(self, text = "Quit", command= self.quit)
-        self.quit.grid(row= 0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        #self.rowconfigure(1, weight=1)
+        self.quit = Button(self, text = "Quit", command= self.quit,padx=10, pady=10, fg = "red")
+        self.btn = Button(self, text = "1", pady = 10, padx= 10)
+        self.btn2 = Button(self, text = "2", pady = 10, padx= 10)
+        
+        self.btn.grid(row = 0, column= 0, sticky = "NSEW")
+        self.btn2.grid(row= 0, column = 1)
+        
+        self.pack(pady = 15, fill = "x")
+        #self.quit.pack(padx=20, pady = 20)
+
         
 app = App()
+app.master.config(bg = "gray")
 app.master.title("Password Maker")
 app.mainloop()
 
